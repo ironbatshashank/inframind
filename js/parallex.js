628413f4d7286bbb191f5ed6e301011a394bd7fe
Avinashkr1612 <avinashkr1612@gmail.com>
2018-12-18 20:28:39 +0530

    Front end

---
+++ js/parallex.js
@@ -0,0 +1,22 @@
+/*  $(window).scroll(function(){
+    
+    var wScroll = $(this).scrollTop();
+    console.log(wScroll);
+    $('.parallax--bg').css('background-position','center '+(wScroll*0.75)+'px');
+    $('.parallax--box').css('top',-5+(wScroll*0.005)+'em');
+})
+ 
+
+
+/*
+(function(){
+    parallaxbg=$('.parallax--bg');
+    document.on('scroll',function(){
+        var scrollPos = $(document).scrollTop();
+        parallaxbg.css('background-position','0' + -scrollPos/4 +'px');
+    })
+
+
+});
+
+*/
